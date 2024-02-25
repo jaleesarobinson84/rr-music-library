e0f24079366821b52ae3fecec1bd90877a6c2434
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-02-25 11:23:58 -0500

    ArtistView and AlbumView files completed

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
 function ArtistView() {
+    const navigate = useNavigate()
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
@@ -35,13 +36,25 @@ function ArtistView() {
         </div>
     )
     
-  
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
 
-
-
-
-
-
+    return (
+        <div>
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
+            {navButtons()}
+            {renderAlbums}
+        </div>
+    )
 }
+
+
 
 export default ArtistView
